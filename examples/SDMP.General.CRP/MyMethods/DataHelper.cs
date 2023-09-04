392cb4a87e595f9e398b2a9c9b48ec9ae935fa6c
Sungwon Hong <swonhong.github@gmail.com>
2023-09-04 13:29:28 +0900

    Add Copyright header

--- examples/SDMP.General.CRP/MyMethods/DataHelper.cs
+++ examples/SDMP.General.CRP/MyMethods/DataHelper.cs
@@ -1,4 +1,8 @@
-﻿using Nodez.Data.DataModel;
+﻿// Copyright (c) 2021-23, Sungwon Hong. All Rights Reserved. 
+// This Source Code Form is subject to the terms of the Mozilla Public License, Version 2.0. 
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+using Nodez.Data.DataModel;
 using Nodez.Data.Interface;
 using Nodez.Data.Managers;
 using SDMP.General.CRP.MyInputs;
