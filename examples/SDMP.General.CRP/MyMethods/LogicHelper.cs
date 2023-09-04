35138d8d512054bfea7e876710c2029cc7f97c8c
Sungwon Hong <swonhong.github@gmail.com>
2023-09-04 12:42:12 +0900

    Commit

--- examples/SDMP.General.CRP/MyMethods/LogicHelper.cs
+++ examples/SDMP.General.CRP/MyMethods/LogicHelper.cs
@@ -1,8 +1,4 @@
-﻿// Copyright (c) 2021-23, Sungwon Hong. All Rights Reserved. 
-// This Source Code Form is subject to the terms of the Mozilla Public License, Version 2.0. 
-// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-using SDMP.General.CRP.MyObjects;
+﻿using SDMP.General.CRP.MyObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
