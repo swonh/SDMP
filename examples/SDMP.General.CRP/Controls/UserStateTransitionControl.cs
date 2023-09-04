ee0ee580055f9385d1ce2bd0c118cd2094b17ecd
Sungwon Hong <swonhong.github@gmail.com>
2023-09-04 12:42:19 +0900

    Commit.

--- examples/SDMP.General.CRP/Controls/UserStateTransitionControl.cs
+++ examples/SDMP.General.CRP/Controls/UserStateTransitionControl.cs
@@ -1,4 +1,8 @@
-﻿using Nodez.Sdmp.Enum;
+﻿// Copyright (c) 2021-23, Sungwon Hong. All Rights Reserved. 
+// This Source Code Form is subject to the terms of the Mozilla Public License, Version 2.0. 
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+using Nodez.Sdmp.Enum;
 using Nodez.Sdmp.General.Controls;
 using Nodez.Sdmp.General.DataModel;
 using System;
@@ -17,7 +21,7 @@ namespace SDMP.General.CRP.Controls
 
         public override List<StateTransition> GetStateTransitions(StateActionMap stateActionMap)
         {
-            return base.GetStateTransitions(stateActionMap); 
+            return base.GetStateTransitions(stateActionMap);
         }
     }
 }
