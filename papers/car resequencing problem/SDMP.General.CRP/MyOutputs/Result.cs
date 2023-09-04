71d80ed05841812936db749076734dea6ccd37dc
Sungwon Hong <swonhong.github@gmail.com>
2023-09-04 15:15:43 +0900

    Add paper crp example code

---
+++ papers/car resequencing problem/SDMP.General.CRP/MyOutputs/Result.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) 2021-23, Sungwon Hong. All Rights Reserved. 
+// This Source Code Form is subject to the terms of the Mozilla Public License, Version 2.0. 
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+using Nodez.Data.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDMP.General.CRP.MyOutputs
+{
+    public class Result : IOutputRow
+    {
+        // Define columns here (NOTICE: The column name defined here and the column name defined in the data file must match.)
+
+        public int SEQUENCE { get; set; }
+
+        public int CONVEYOR { get; set; }
+
+        public int JOB { get; set; }
+
+        public int COLOR { get; set; }
+
+    }
+}
