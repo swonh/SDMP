82e6a9a4bd60851c29fbae0aaea7d9729b169595
Sungwon Hong <swonhong.github@gmail.com>
2024-10-28 18:03:27 +0900

    Release v1.0.6.

---
+++ src/Nodez.Package/obj/Release/ProjectTemplates/SDMP Scheduling Project Template/Controls/Scheduling/UserSchedulingControl.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2021-24, Sungwon Hong. All Rights Reserved. 
+// This Source Code Form is subject to the terms of the Mozilla Public License, Version 2.0. 
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+using Nodez.Sdmp.Enum;
+using Nodez.Sdmp.General.Managers;
+using Nodez.Sdmp.Scheduling.Controls;
+using Nodez.Sdmp.Scheduling.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $safeprojectname$.Controls
+{
+    public class UserSchedulingControl : SchedulingControl
+    {
+        private static readonly Lazy<UserSchedulingControl> lazy = new Lazy<UserSchedulingControl>(() => new UserSchedulingControl());
+
+        public static new UserSchedulingControl Instance { get { return lazy.Value; } }
+
+        public override ScheduleType GetScheduleType() 
+        {
+            return ScheduleType.EqpScheduling;
+        }
+    }
+}
